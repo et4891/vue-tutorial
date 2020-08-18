b295d8ce7f4254ffcdaabf3ab7a5c7bcc6bb6687
et4891 <et1103@hotmail.com>
2020-08-18 09:23:35 -0700

    2. Attribute binding

--- vue_mastery/js/main.js
+++ vue_mastery/js/main.js
@@ -1,6 +1,9 @@
 const app = new Vue({
     el: '#app',
     data: {
-        product: `Socks`
+        product: `Socks`,
+        product_image: './assets/vmSocks-green.jpg',
+        product_link: 'https://www.google.com',
+        alt_text: 'A pair of socks'
     }
 });
