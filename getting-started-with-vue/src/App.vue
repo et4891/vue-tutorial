9cbc705696adc9b56cb4035279ec043e83675ea6
et4891 <et1103@hotmail.com>
2020-08-18 09:51:27 -0700

    fix employee id dupliate error

--- getting-started-with-vue/src/App.vue
+++ getting-started-with-vue/src/App.vue
@@ -25,7 +25,7 @@ export default {
   methods: {
     addEmployee(employee){
       // since id start with 1, if there's no employees, start id with 1, else check length of array and increase by 1
-      const lastId = (this.employees.length > 0) ? this.employees.length : 1;
+      const lastId = (this.employees.length > 0) ? this.employees.length : 0;
       const newEmployee = {...employee, id: lastId + 1};
       this.employees = [...this.employees, newEmployee];
     }
