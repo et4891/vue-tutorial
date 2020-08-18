afcf24fb7538c53fced25ca7ebd5306599ff9386
et4891 <et1103@hotmail.com>
2020-08-18 09:26:46 -0700

    6. class & style binding

--- vue_mastery/js/main.js
+++ vue_mastery/js/main.js
@@ -5,7 +5,8 @@ const app = new Vue({
         product_image: './assets/vmSocks-green.jpg',
         product_link: 'https://www.google.com',
         alt_text: 'A pair of socks',
-        inventory: 100,
+        inventory: 10,
+        inStock: true,
         onSaleShow: false,
         onSaleImage: './assets/on-sale.png',
         details: [
@@ -17,11 +18,13 @@ const app = new Vue({
             {
               variantId: 2234,
               variantColor: 'green',
+              variantColorHex: '#39a741',
               variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
             },
             {
               variantId: 2235,
               variantColor: 'blue',
+              variantColorHex: '#466383',
               variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
             }
           ],
@@ -29,7 +32,15 @@ const app = new Vue({
         },
         methods: {
           addToCart() {
-            this.cart += 1
+            this.cart++;
+            this.inventory--;
+            if(this.inventory === 0) return this.inStock = false;
+          },
+          removeFromCart(){
+            if(this.cart === 0) return;
+            this.cart--;
+            this.inventory++;
+            if(this.inventory > 0) this.inStock = true;
           },
           updateProduct(variantImage) {
             this.product_image = variantImage
