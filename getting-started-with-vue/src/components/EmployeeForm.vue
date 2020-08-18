cca0529bdbad2275609e57fbae4ff6bf9a0adc3a
et4891 <et1103@hotmail.com>
2020-08-18 09:39:03 -0700

    add event listener

--- getting-started-with-vue/src/components/EmployeeForm.vue
+++ getting-started-with-vue/src/components/EmployeeForm.vue
@@ -1,33 +1,45 @@
 <template>
-    <div id="employee-form">
-        <form>
-            <label for="">Employee Name</label>
-            <input v-model='employee.name' type="text" />
-            <label for="">Employee Email</label>
-            <input v-model='employee.email' type="text" />
-            <button>Add Employee</button>
-        </form>
-    </div>
+  <div id="employee-form">
+    <form @submit.prevent='handleSubmit'>
+      <label for="">Employee Name</label>
+      <input
+        v-model='employee.name'
+        type="text"
+      />
+      <label for="">Employee Email</label>
+      <input
+        v-model='employee.email'
+        type="text"
+      />
+      <button>Add Employee</button>
+    </form>
+  </div>
 </template>
 
 
 <script>
 export default {
-    name: 'employee-form',
-    data(){
-        return {
-            employee: {
-                name: '',
-                email: '',
-            }
-        }
+  name: "employee-form",
+  data() {
+    return {
+      employee: {
+        name: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const { name, email } = this.employee;
+      console.log(name, "name");
+      console.log(email, "email");
     }
-
-}
+  }
+};
 </script>
 
 <style scoped>
-    form {
-        margin-bottom: 2rem;
-    }
+form {
+  margin-bottom: 2rem;
+}
 </style>
