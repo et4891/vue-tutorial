8cc387386e3004e2b69da9542cdfc7c768948d5d
et4891 <et1103@hotmail.com>
2020-08-18 09:36:37 -0700

    loop

---
+++ getting-started-with-vue/src/components/EmployeeTable.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for='employee in employees' :key='employee.id'>
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+        </tr>
+        <!-- <tr>
+          <td>Richard Hendricks</td>
+          <td>richard@piedpiper.com</td>
+        </tr>
+
+        <tr>
+          <td>Bertram Gilfoyle</td>
+          <td>gilfoyle@piedpiper.com</td>
+        </tr>
+
+        <tr>
+          <td>Dinesh Chugtai</td>
+          <td>dinesh@piedpiper.com</td>
+        </tr> -->
+      </tbody>
+
+    </table>
+  </div>
+</template>
+
+
+<script>
+	export default {
+    name: 'employee-table',
+    props: {
+      employees: Array
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
